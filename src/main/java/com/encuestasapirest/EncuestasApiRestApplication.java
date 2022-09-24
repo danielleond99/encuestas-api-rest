4529550fd01853b7a9ec44bcff7ddb4d5b80c58e
danielleond99 <danielleond99@gmail.com>
2022-09-24 17:25:42 -0400

    init commit

---
+++ src/main/java/com/encuestasapirest/EncuestasApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.encuestasapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestasApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestasApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encuestasapirest/EncuestasApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encuestasapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestasApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
